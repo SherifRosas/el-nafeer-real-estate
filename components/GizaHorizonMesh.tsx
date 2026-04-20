f558fbd7be04c53e1497f9941267e71992c83cb8
SherifRosas <sherifrosas.ai@gmail.com>
2026-04-20 07:32:13 +0200

    fix(portal): resolve build error by importing useState in GizaHorizonMesh

--- components/GizaHorizonMesh.tsx
+++ components/GizaHorizonMesh.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef, useMemo } from 'react'
+import React, { useRef, useMemo, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { 
     PerspectiveCamera, 
